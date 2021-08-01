36384cc116da3956386e6b9231578d2530b9c572
Paulo Oliveros <V8T2Z6L@johndeere.com>
2021-07-31 21:07:47 -0300

    Componentizacao

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+import '../styles/content.scss';
+
+export interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  category: string; 
+  movies: MovieProps[];
+}
+
+export function Content(props: ContentProps) {
+    
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.category}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
